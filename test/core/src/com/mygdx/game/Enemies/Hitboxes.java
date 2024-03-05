bd84137a9dcf6037e2f96c53c6f0bd57a56dfb16
WeihaoWu1 <144487425+WeihaoWu1@users.noreply.github.com>
2024-03-05 15:35:31 -0500

    Add files via upload

---
+++ test/core/src/com/mygdx/game/Enemies/Hitboxes.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.Enemies;
+
+import java.awt.geom.Rectangle2D;
+
+public class Hitboxes {
+
+    private float x ,y;
+    private int width, height;
+    protected Rectangle2D.Float hitBox;
+    public Hitboxes(float x, float y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    protected void initializeHitBox(float x, float y, float width, float height) {
+        hitBox = new Rectangle2D.Float((int) x, (int) y, width, height);
+    }
+}
