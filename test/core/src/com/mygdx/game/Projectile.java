c6c9871be75a4019dcd39501f9a3d03644dc5bf6
Raymondw923368 <144486921+Raymondw923368@users.noreply.github.com>
2024-03-05 23:58:56 -0500

    Add files via upload

---
+++ test/core/src/com/mygdx/game/Projectile.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+import com.mygdx.game.FireTowerAnimation;
+import com.mygdx.game.RockTowerAnimation;
+public class Projectile {
+    private String type;
+    public Projectile(String type){
+        this.type = type;
+    }
+
+    public void homing(int targetX, int targetY){
+
+    }
+}
