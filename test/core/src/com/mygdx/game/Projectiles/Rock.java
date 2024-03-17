e2147aa73bcb38aab5fe80c9569c2e687040c3ff
WeihaoWu1 <144487425+WeihaoWu1@users.noreply.github.com>
2024-03-17 01:55:25 -0400

    Add files via upload

---
+++ test/core/src/com/mygdx/game/Projectiles/Rock.java
@@ -0,0 +1,121 @@
+package com.mygdx.game.Projectiles;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.Enemies.Hitboxes;
+import com.mygdx.game.Enemies.Slime;
+import com.mygdx.game.Screens.PlayScreen;
+import com.mygdx.game.Towers.FireTowerAnimation;
+import com.mygdx.game.Towers.RockTowerAnimation;
+import com.mygdx.game.gdxGame;
+
+public class Rock {
+    Slime shortestSlime;
+    private float initialX;
+    private float initialY;
+    long lastAttack = 0;
+    long coolDownTime = 1000;
+    private float shortest = Float.MAX_VALUE;
+    Texture rockicon = new Texture("rock.png");
+    Sprite rock;
+    private Rectangle hitBox;
+    public Rock(float x, float y, int width, int height) {
+        initialX = x;
+        initialY = y;
+    }
+    public void create(){
+        rock = new Sprite(rockicon);
+        hitBox = rock.getBoundingRectangle();
+        hitBox.setX(initialX+80);
+        hitBox.setY(initialY+40);
+    }
+
+    public void render(){
+        gdxGame.batch.draw(rock, hitBox.x+80, hitBox.y);
+    }
+    public Slime getClosest(){
+        for (Slime s : PlayScreen.slimes){
+            float xDist = Math.abs(s.getX() - initialX);
+            float yDist = Math.abs(s.getY() - initialY);
+            float realDist = (float) Math.hypot(xDist, yDist);
+            if (realDist < shortest) {
+                shortest = realDist;
+                shortestSlime = s;
+            }
+        }
+        return shortestSlime;
+    }
+
+
+    public void cooldown() {
+        long time = System.currentTimeMillis();
+        if (time > lastAttack + coolDownTime) {
+            // Do something
+            lastAttack = time;
+        }
+    }
+    public void intersects(RockTowerAnimation r) {
+        if (Intersector.overlaps(hitBox, getClosest().getHitBox())) {
+            getClosest().health -= 20;
+            if (getClosest().health <= 0) {
+                getClosest().setX(-99999999f);
+                getClosest().setY(-99999999f);
+                for (Rock e : r.getRocks()) {
+                    e.setX(-9999999);
+                    e.setY(-9999999);
+                }
+            }
+        }
+    }
+
+    public void shoot(RockTowerAnimation r) {
+        if (Intersector.overlaps(r.getHitBox(), getClosest().getHitBox())) {
+            float xIncrement;
+            float yIncrement;
+            float xDist = Math.abs(shortestSlime.getX() - initialX);
+            float yDist = Math.abs(shortestSlime.getY() - initialY);
+            float realDist = (float) Math.hypot(xDist, yDist);
+            float ratioy = yDist / realDist;
+            float ratiox = xDist / realDist;
+            xIncrement = realDist * (ratiox / 10);
+            yIncrement = realDist * (ratioy / 10);
+            if (shortestSlime.getX() < initialX && shortestSlime.getY() > initialY) {
+
+                hitBox.x -= xIncrement;
+                hitBox.y += yIncrement;
+            }
+            if (shortestSlime.getX() > initialX && shortestSlime.getY() > initialY) {
+                hitBox.x += xIncrement;
+                hitBox.y += yIncrement;
+            }
+            if (shortestSlime.getX() < initialX && shortestSlime.getY() < initialY) {
+                hitBox.x -= xIncrement;
+                hitBox.y -= yIncrement;
+
+            }
+            if (shortestSlime.getX() > initialX && shortestSlime.getY() < initialY) {
+                hitBox.x += xIncrement;
+                hitBox.y -= yIncrement;
+            }
+        }
+    }
+    public Rectangle getHitBox(){
+        return hitBox;
+    }
+
+    public void setX(float newX){
+        hitBox.x = newX;
+    }
+
+    public void setY(float newY){
+        hitBox.y= newY;
+    }
+
+
+    public void dispose(){
+        rock.getTexture().dispose();
+    }
+
+}
---
+++ test/core/src/com/mygdx/game/Towers/FireTowerAnimation.java
@@ -0,0 +1,144 @@
+package com.mygdx.game.Towers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Intersector;
+import com.mygdx.game.Enemies.Hitboxes;
+import com.mygdx.game.Enemies.Slime;
+import com.mygdx.game.Projectiles.Fire;
+import com.mygdx.game.Projectiles.Rock;
+import com.mygdx.game.Screens.PlayScreen;
+import com.mygdx.game.gdxGame;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+
+public class FireTowerAnimation extends TowerHitbox {
+    // Constant rows and columns of the sprite sheet
+    private static final int FRAME_COLS = 24, FRAME_ROWS = 1;
+
+    // Objects used
+    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+    Texture walkSheet;
+    SpriteBatch spriteBatch;
+
+    // A variable for tracking elapsed time for the animation
+    float stateTime;
+
+    private  ArrayList<Fire> fires = new ArrayList<Fire>();
+
+    private float increment;
+
+
+    public FireTowerAnimation (float x, float y, float radius){
+        super(x+80,y+40,radius);
+        initializeHitBox(x,y,radius);
+    }
+
+    public ArrayList<Fire> getFires(){
+        return fires;
+    }
+
+    public void spawnProjectile() {
+        for (Slime s : PlayScreen.slimes) {
+            if (Intersector.overlaps(hitBox, s.getHitBox())){
+                Fire fire = new Fire((float) this.getX()-40 , (float) this.getY()-40, 100, 100);
+                fires.add(fire);
+                fire.create();
+//                s.health -= 20;
+//                if (s.health <= 0) {
+//                    s.setX(-9999f);
+//                    s.setY(-9999f);
+//                }
+            }
+        }
+//        for (int f=0; f < fires.size(); f++) {
+//            if (Intersector.overlaps(fires.get(f).getHitBox(), fires.get(f).getClosest().getHitBox())){
+////                    fires.get(f).setX(-9999f);
+////                    fires.get(f).setY(-9999f);
+//                fires.get(f).getClosest().health -= 50;
+//                if (fires.get(f).getClosest().health <= 0) {
+//                    fires.get(f).getClosest().setX(-9999999f);
+//                    fires.get(f).getClosest().setY(-9999999f);
+//                }
+////                fires.get(f).getClosest();
+////                fires.remove(f);
+////                f--;
+//        }
+
+    }
+//        for(int i=fires.size()-1;i>2;i--){
+//            fires.remove(i);
+//        }
+
+//        for (RockTowerAnimation r : PlayScreen.rocktowers){
+//            if (hitBox.intersects(r.hitBox)){
+//                PlayScreen.rocks.add(new Rock(r.getX(), r.getY(), 5, 5));
+//            }
+//        }
+
+
+    public void create() {
+
+        // Load the sprite sheet as a Texture
+        walkSheet = new Texture(Gdx.files.internal("fire_tower.png"));
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
+                walkSheet.getWidth() / FRAME_COLS,
+                walkSheet.getHeight() / FRAME_ROWS);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+
+        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
+        // time to 0
+//        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+    }
+
+    public void setX(float x){
+        hitBox.x = x;
+        initializeHitBox(hitBox.x,hitBox.y,hitBox.radius);
+    }
+    public void setY(float y){
+        hitBox.y = y;
+        initializeHitBox(hitBox.x,hitBox.y,hitBox.radius);
+    }
+
+
+    public void render() {
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+        stateTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        gdxGame.batch.draw(currentFrame, hitBox.x-80, hitBox.y-80);
+//        hitBox.setFrame(100.0d, 100.0d, 80.0d, 120.0d);
+//        gdxGame.batch.draw(hitBox);
+    }
+
+//    public void drawHitboxes(Graphics g){
+//        Graphics2D g1 = (Graphics2D)g;
+//        g1.setColor(Color.BLUE);
+//        g1.draw(hitBox);
+//    }
+
+    public void dispose() { // SpriteBatches and Textures must always be disposed
+        walkSheet.dispose();
+    }
+}
